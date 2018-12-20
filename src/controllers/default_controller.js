de43ecf1b8ec2fbb06e5c3e8043d654f7175b8bb
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2018-12-20 11:01:36 +0530

    updated fortjs version

--- src/controllers/default_controller.js
+++ src/controllers/default_controller.js
@@ -3,19 +3,20 @@ import {
     defaultWorker,
     htmlResult,
     textResult,
-    renderView
+    renderView,
+    viewResult
 } from "fortjs";
 
 export class DefaultController extends Controller {
     @defaultWorker()
     async default () {
         try {
-            const viewData = await renderView('controller:default,action:default');
-            const result = await htmlResult(viewData);
+            const result = await viewResult('controller:default,action:default');
             return result;
         } catch (ex) {
             console.log(ex);
             // handle exception and show user a good message.
+            // save this ex in a file or db, so that you can know what's the issue and where
             const result = await textResult(`Our server is busy right now. Please try later.`);
             return result;
         }
